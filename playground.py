b04e6e4035a8823e467cf1f5728cebe1dd71362a
bonyvah <bekbolsunysmanov07@gmail.com>
2026-03-16 12:18:46 +0600

    rm playground.py from tracking

--- playground.py
+++
@@ -1,8 +0,0 @@
-
-a = "https://www.youtube.com/watch?v=wTT8QaE0nnw&t=2s"
-b ="https://youtu.be/wTT8QaE0nnw?si=9nWDXr4T-1R7UUNE"
-
-if "youtube" in a: 
-  print(a[a.index("=")+1:a.index("&t")])
-if "youtu.be" in b:
-  print(b[b.index(".be/") + 4:b.index("?si")])
